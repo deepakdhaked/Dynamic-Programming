4acf04e084ed864978b065ecf274b1ddd742353b
deepak dhaked <deepakdhakedpc@gmail.com>
2023-05-31 10:13:15 +0530

    fibonacci memoization is added

---
+++ fibonacci_memoization.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Time Comlexity : O(N)
+// Space Comlexity : O(N)
+int fibonacci_memoization(int n, vector<int> &dp)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    dp[n] = fibonacci_memoization(n - 1, dp) + fibonacci_memoization(n - 2, dp);
+    return dp[n];
+}
+
+int main()
+{
+    int n = 8;
+    vector<int> dp(n + 1, -1);
+    cout << fibonacci_memoization(n, dp) << endl;
+}
